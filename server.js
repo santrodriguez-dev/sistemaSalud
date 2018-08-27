b109d48e9ad7970421b0a6ace0522360509b8890
Santiago Rodríguez <roms_195@hotmail.com>
2018-08-27 00:25:23 -0500

    v

--- server.js
+++ server.js
@@ -14,3 +14,4 @@ res.sendFile(path.join(__dirname+'/dist/sistema-salud/index.html'));
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
+console.log("Servidor corriendo en el puerto 8080")
